474a953f9069f3b2568a9adc90d944beb516fbab
Natalie Chen <nsc9331@nyu.edu>
2026-06-29 00:20:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+import shap
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 st.set_page_config(
     page_title="Flight Price Predictor ✈️",
